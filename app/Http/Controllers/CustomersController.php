991f740eff6fa9ba98167a22d3e0f3423cceac18
whitc2219 <whitc@earthlink.net>
2019-08-05 08:23:46 -0400

    test

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -9,7 +9,7 @@ class CustomersController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+    //    $this->middleware('auth');
     }
 
     public function list() {
