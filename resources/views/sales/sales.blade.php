c47ad158edce9f8da5e0c4ba02915279bf74f1d4
whitc2219 <whitc@earthlink.net>
2019-08-06 09:17:09 -0400

    test

--- resources/views/sales/sales.blade.php
+++ resources/views/sales/sales.blade.php
@@ -39,7 +39,7 @@
 
 @section('content')
     <!-- Page Content -->
-    <div class="content">
+    <div>
         <!--class="content"-->
         <div class="my-50 text-center">
             <h2 class="font-w700 text-black mb-10">Legacy Maintenance Services</h2>
