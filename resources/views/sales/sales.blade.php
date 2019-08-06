857ea35b0e49888ef4d55690474a5365c47f62b0
whitc2219 <whitc@earthlink.net>
2019-08-06 09:17:49 -0400

    test

--- resources/views/sales/sales.blade.php
+++ resources/views/sales/sales.blade.php
@@ -69,7 +69,7 @@
             <div class="block-content block-content-full">
                 <!--class="block-content block-content-full"-->
                 <!-- DataTables init on table by adding .js-dataTable-full class, functionality is initialized at the top of this page -->
-                <table style="width: 100%">
+                <table class="table table-bordered table-striped table-vcenter js-dataTable-full" style="width: 100%">
                     <!-- class="table table-bordered table-striped table-vcenter js-dataTable-full" -->
                     <thead>
                     <tr>
