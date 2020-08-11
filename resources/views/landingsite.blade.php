e80960ad48dbcd1bf6f115d79b35bdc0d70bcf83
whitc2219 <whitc@earthlink.net>
2020-08-11 11:46:20 -0400

    test

--- resources/views/landingsite.blade.php
+++ resources/views/landingsite.blade.php
@@ -170,7 +170,6 @@
 
                     <span class="fa-stack fa-2x">
   <i class="fas fa-circle-thin fa-stack-2x"></i>
-  <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
 </span>
 
                 </div>	<!-- End Navigation Menu -->
