ce9e3d8b893bde65df47fcaee3463ff7ed375d07
whitc2219 <whitc@earthlink.net>
2020-08-11 10:28:43 -0400

    test

--- resources/views/landingsite.blade.php
+++ resources/views/landingsite.blade.php
@@ -153,8 +153,8 @@
                         <li class="nav-item nl-simple"><a class="nav-link" href="#reviews-2">Reviews</a></li>
                         <li class="nav-item nl-simple"><a class="nav-link" href="#faqs-2">Gallery</a></li>
 
-                        <li><a style="padding-right: 10px; padding-top: 6px;" href="#" class="ico-facebook"><i class="fab fa-facebook-f"></i></a></li>
-                        <li><a style="padding-top: 6px;" href="#" class="ico-twitter"><i class="fab fa-instagram"></i></a></li>
+                        <li><a style="color: white; padding-right: 10px; padding-top: 6px;" href="#" class="ico-facebook"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a style="color: white; padding-top: 6px;" href="#" class="ico-twitter"><i class="fab fa-instagram fa-2x"></i></a></li>
 
                     </ul>
 
